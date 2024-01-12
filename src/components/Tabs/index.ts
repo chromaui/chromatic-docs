9340aba9b503476c2619f215f2a7f5c48458f60a
winkerVSbecks <varunvachhar@gmail.com>
2024-01-12 12:18:06 -0500

    adds a tab comonent to specify multiple code snippets within a block

---
+++ src/components/Tabs/index.ts
@@ -0,0 +1,9 @@
+/**
+ * This Tab component is based on
+ * https://github.com/withastro/starlight/tree/main/packages/starlight/user-components
+ *
+ * Why not use the original?
+ * To avoid adding a dependency on an entire theme just for one component
+ */
+export { default as TabItem } from "./TabItem.astro";
+export { default as Tabs } from "./Tabs.astro";
