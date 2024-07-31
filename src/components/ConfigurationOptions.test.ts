e6c735679a5016f52623eeb9a91e33b196451396
winkerVSbecks <varunvachhar@gmail.com>
2024-07-31 13:52:35 -0400

    upgrade astro

---
+++ src/components/ConfigurationOptions.test.ts
@@ -0,0 +1,8 @@
+import { expect, test, describe } from "vitest";
+import { formatOption } from "./ConfigurationOptions.astro";
+
+describe("Format Option", () => {
+  test("Creates a schema", async () => {
+    // formatOption({});
+  });
+});
