ae4db5528a252205fca53577cf91d94495bf5a58
winkerVSbecks <varunvachhar@gmail.com>
2025-06-18 15:38:16 -0400

    update billing info for a11y tests and add snapshot calculator

---
+++ src/components/SnapshotCalculator.test.ts
@@ -0,0 +1,158 @@
+import { expect, test, describe } from "vitest";
+import { calculateSnapshots } from "./SnapshotCalculator";
+
+const examples = [
+  {
+    tests: 1,
+    builds: 10,
+    browsers: 1,
+    viewports: 1,
+    snapshots: 10,
+  },
+  {
+    tests: 1,
+    builds: 10,
+    browsers: 2,
+    viewports: 1,
+    snapshots: 20,
+  },
+  {
+    tests: 2,
+    builds: 10,
+    browsers: 1,
+    viewports: 1,
+    snapshots: 20,
+  },
+  {
+    tests: 2,
+    builds: 10,
+    browsers: 2,
+    viewports: 1,
+    snapshots: 40,
+  },
+  {
+    tests: 2,
+    builds: 10,
+    browsers: 2,
+    viewports: 2,
+    snapshots: 80,
+  },
+];
+
+const turboSnapExamples = [
+  {
+    tests: 50,
+    builds: 1,
+    browsers: 1,
+    viewports: 1,
+    changedTests: 50,
+    snapshots: 50,
+    turboSnaps: 0,
+    billedSnapshots: 50,
+  },
+  {
+    tests: 50,
+    builds: 1,
+    browsers: 1,
+    viewports: 1,
+    changedTests: 10,
+    snapshots: 10,
+    turboSnaps: 40,
+    billedSnapshots: 18,
+  },
+  {
+    tests: 50,
+    builds: 1,
+    browsers: 2,
+    viewports: 1,
+    changedTests: 50,
+    snapshots: 100,
+    turboSnaps: 0,
+    billedSnapshots: 100,
+  },
+  {
+    tests: 50,
+    builds: 1,
+    browsers: 2,
+    viewports: 1,
+    changedTests: 10,
+    snapshots: 20,
+    turboSnaps: 80,
+    billedSnapshots: 36,
+  },
+  {
+    tests: 50,
+    builds: 1,
+    browsers: 2,
+    viewports: 2,
+    changedTests: 10,
+    snapshots: 40,
+    turboSnaps: 160,
+    billedSnapshots: 72,
+  },
+];
+
+describe("calculateSnapshots", () => {
+  test("Calculates snapshots", () => {
+    examples.forEach(({ tests, builds, browsers, viewports, snapshots }) => {
+      expect(
+        calculateSnapshots(tests, builds, browsers, viewports, false, false),
+      ).toEqual({
+        snapshots,
+        turboSnaps: 0,
+        billedSnapshots: snapshots,
+      });
+    });
+  });
+
+  test("Calculates snapshots with accessibility enabled", () => {
+    examples.forEach(({ tests, builds, browsers, viewports, snapshots }) => {
+      expect(
+        calculateSnapshots(tests, builds, browsers, viewports, true, false),
+      ).toEqual({
+        snapshots: snapshots * 2,
+        turboSnaps: 0,
+        billedSnapshots: snapshots * 2,
+      });
+    });
+  });
+
+  test("Calculates snapshots with TurboSnap enabled", () => {
+    turboSnapExamples.forEach(
+      ({
+        tests,
+        builds,
+        browsers,
+        viewports,
+        snapshots,
+        turboSnaps,
+        billedSnapshots,
+        changedTests,
+      }) => {
+        expect(
+          calculateSnapshots(
+            tests,
+            builds,
+            browsers,
+            viewports,
+            false,
+            true,
+            changedTests,
+          ),
+        ).toEqual({
+          snapshots,
+          turboSnaps,
+          billedSnapshots,
+        });
+      },
+    );
+  });
+
+  test("Calculates snapshots with TurboSnap enabled", () => {
+    expect(calculateSnapshots(50, 1, 2, 2, true, true, 10)).toEqual({
+      billedSnapshots: 112,
+      snapshots: 80,
+      turboSnaps: 160,
+    });
+  });
+});
---
+++ src/components/SnapshotCalculator.tsx
@@ -0,0 +1,279 @@
+import { useEffect, useState } from "react";
+import { styled } from "@storybook/theming";
+import {
+  spacing,
+  color,
+  VStack,
+  minSm,
+  fontWeight,
+  Stat,
+  typography,
+  fontFamily,
+  Icon,
+} from "@chromatic-com/tetra";
+
+export function calculateSnapshots(
+  tests: number,
+  builds: number,
+  browsers: number,
+  viewports: number,
+  accessibility: boolean,
+  turboSnapEnabled: boolean,
+  changedTests: number = 0,
+): {
+  snapshots: number;
+  turboSnaps: number;
+  billedSnapshots: number;
+} {
+  const baseSnapshots = tests * builds * browsers * viewports;
+  const accessibilitySnapshots = accessibility ? baseSnapshots : 0;
+  const totalSnapshots = baseSnapshots + accessibilitySnapshots;
+
+  if (!turboSnapEnabled) {
+    return {
+      snapshots: totalSnapshots,
+      turboSnaps: 0,
+      billedSnapshots: totalSnapshots,
+    };
+  }
+
+  const unchangedTests = tests - changedTests;
+
+  const regularSnapshots = changedTests * builds * browsers * viewports;
+  const regularA11ySnapshots = accessibility ? regularSnapshots : 0;
+  const totalRegularSnapshots = regularSnapshots + regularA11ySnapshots;
+
+  const turboSnaps = unchangedTests * builds * browsers * viewports;
+
+  // Total billed snapshots is the sum of regular and turbo snapshots
+  const billedSnapshots = totalRegularSnapshots + 0.2 * turboSnaps;
+
+  return {
+    snapshots: totalRegularSnapshots,
+    turboSnaps,
+    billedSnapshots: Math.round(billedSnapshots),
+  };
+}
+
+const Container = styled.div`
+  padding: ${spacing[4]};
+  border-radius: 4px;
+  margin-bottom: 1.5em;
+  border: 1px solid ${color.blackTr10};
+`;
+
+const Field = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: ${spacing[2]};
+
+  ${minSm} {
+    flex-direction: row;
+    align-items: center;
+  }
+
+  label {
+    font-weight: ${fontWeight.bold};
+    min-width: 40%;
+  }
+
+  input {
+    flex: 1;
+    min-width: auto;
+    border: 1px solid ${color.blackTr10};
+    border-radius: 4px;
+    padding: ${spacing[1]};
+  }
+`;
+
+const Checkbox = styled.div`
+  display: flex;
+  align-items: center;
+  gap: ${spacing[2]};
+`;
+
+const Formula = styled.div`
+  ${typography.body14}
+  font-size: 12px;
+  font-family: ${fontFamily.mono};
+  color: ${color.slate600};
+  display: flex;
+  align-items: center;
+  gap: ${spacing[2]};
+  border-top: 1px solid ${color.blackTr10};
+  padding: ${spacing[4]};
+  margin-top: ${spacing[4]};
+  margin-left: -${spacing[4]};
+  margin-right: -${spacing[4]};
+`;
+
+const InfoStat = styled(Stat)`
+  padding: ${spacing[2]};
+  background: #f8f8f8;
+  border-radius: 4px;
+`;
+
+const BillingStat = styled(Stat)`
+  padding: ${spacing[2]};
+  background: ${color.blue100};
+  border-radius: 4px;
+`;
+
+const Result = styled.div`
+  border-top: 1px solid ${color.blackTr10};
+  margin-left: -${spacing[4]};
+  margin-right: -${spacing[4]};
+  padding: ${spacing[4]} ${spacing[4]} 0;
+  display: flex;
+  gap: ${spacing[4]};
+
+  > * {
+    flex: 1;
+  }
+`;
+
+export const SnapshotCalculator = () => {
+  const [tests, setTests] = useState(50);
+  const [builds, setBuilds] = useState(1);
+  const [browsers, setBrowsers] = useState(1);
+  const [viewports, setViewports] = useState(1);
+  const [accessibility, setAccessibility] = useState(false);
+  const [turboSnap, setTurboSnap] = useState(false);
+  const [changedTests, setChangedTests] = useState(0);
+
+  useEffect(() => {
+    // Reset changed tests when turboSnap is disabled
+    if (!turboSnap) {
+      setChangedTests(0);
+    } else {
+      // Start with all tests changed if turboSnap is enabled
+      setChangedTests(tests);
+    }
+  }, [turboSnap, setChangedTests]);
+
+  const results = calculateSnapshots(
+    tests,
+    builds,
+    browsers,
+    viewports,
+    accessibility,
+    turboSnap,
+    changedTests,
+  );
+
+  return (
+    <Container>
+      <VStack gap={4}>
+        <Field>
+          <label htmlFor="sc-tests">Number of tests</label>
+          <input
+            id="sc-tests"
+            type="number"
+            value={tests}
+            onChange={(e) => setTests(Number(e.target.value))}
+            min="1"
+          />
+        </Field>
+        <Field>
+          <label htmlFor="sc-builds">Builds</label>
+          <input
+            id="sc-builds"
+            type="number"
+            value={builds}
+            onChange={(e) => setBuilds(Number(e.target.value))}
+            min="1"
+          />
+        </Field>
+        <Field>
+          <label htmlFor="sc-browsers">Browsers</label>
+          <input
+            id="sc-browsers"
+            type="number"
+            value={browsers}
+            onChange={(e) => setBrowsers(Number(e.target.value))}
+            min="1"
+            max="4"
+          />
+        </Field>
+        <Field>
+          <label htmlFor="sc-viewports">Viewports</label>
+          <input
+            id="sc-viewports"
+            type="number"
+            value={viewports}
+            onChange={(e) => setViewports(Number(e.target.value))}
+            min="1"
+          />
+        </Field>
+
+        <VStack gap={1}>
+          <Checkbox>
+            <input
+              id="sc-accessibility"
+              type="checkbox"
+              checked={accessibility}
+              onChange={(e) => setAccessibility(e.target.checked)}
+            />
+            <label htmlFor="sc-accessibility">Accessibility tests</label>
+          </Checkbox>
+          <VStack gap={2}>
+            <Checkbox>
+              <input
+                id="sc-turboSnap"
+                type="checkbox"
+                checked={turboSnap}
+                onChange={(e) => setTurboSnap(e.target.checked)}
+              />
+              <label htmlFor="sc-turboSnap">TurboSnap</label>
+            </Checkbox>
+            {turboSnap && (
+              <Field>
+                <label htmlFor="sc-test-changed">Tests with changes</label>
+                <input
+                  id="sc-test-changed"
+                  type="number"
+                  value={changedTests}
+                  onChange={(e) => setChangedTests(Number(e.target.value))}
+                  min="0"
+                  max={tests}
+                />
+              </Field>
+            )}
+          </VStack>
+        </VStack>
+      </VStack>
+
+      <Formula>
+        <Icon size={12} name="info" /> Snapshot = Tests x Builds x Browsers x
+        Viewports
+      </Formula>
+
+      <Result>
+        <InfoStat
+          unit={`${toPlural(results.snapshots, "Snapshot")} taken`}
+          dimension=""
+          value={results.snapshots.toLocaleString()}
+          size="large"
+        />
+        {turboSnap && (
+          <InfoStat
+            unit={toPlural(results.turboSnaps, "TurboSnap")}
+            dimension=""
+            value={results.turboSnaps.toLocaleString()}
+            size="large"
+          />
+        )}
+        <BillingStat
+          unit={toPlural(results.billedSnapshots, "Billed snapshot")}
+          dimension=""
+          value={results.billedSnapshots.toLocaleString()}
+          size="large"
+        />
+      </Result>
+    </Container>
+  );
+};
+
+function toPlural(count: number, single: string): string {
+  return `${single}${count === 1 ? "" : "s"}`;
+}
