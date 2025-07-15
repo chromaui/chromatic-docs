82d287e9488e5419ae0fd7826a62a98801cd580f
winkerVSbecks <varunvachhar@gmail.com>
2025-07-15 15:43:10 -0400

    fix schema integration

--- chromatic-config/schema-integration.js
+++ chromatic-config/schema-integration.js
@@ -1,10 +1,12 @@
+import { generateSchema } from "./generate-schema.ts";
+import { validateSchema } from "./validate-schema.ts";
+
 export const schemaIntegration = {
   name: "chromatic-config-file-schema",
   hooks: {
     "astro:build:start": async () => {
-      const { generateSchema } = await import("./generate-schema.ts");
       await generateSchema();
-      await import("./validate-schema.ts");
+      await validateSchema();
     },
   },
 };
--- chromatic-config/validate-schema.ts
+++ chromatic-config/validate-schema.ts
@@ -1,6 +1,6 @@
 import { validate } from "@hyperjump/json-schema/draft-2020-12";
 
-async function runValidation(label, content) {
+async function runValidation(label: string, content: any) {
   const output = await validate(
     `./public/chromatic-config.schema.json`,
     content,
@@ -17,101 +17,103 @@ async function runValidation(label, content) {
   }
 }
 
-console.log("🚦 Validating schema for chromatic.config.json file");
+export async function validateSchema() {
+  console.log("🚦 Validating schema for chromatic.config.json file");
 
-runValidation("Minimal", {
-  projectId: "Project:...",
-});
+  await runValidation("Minimal", {
+    projectId: "Project:...",
+  });
 
-runValidation("Unlinked project", {
-  projectId: "Project:...",
-  externals: ["public/**"],
-  junitReport: true,
-  onlyChanged: true,
-});
+  await runValidation("Unlinked project", {
+    projectId: "Project:...",
+    externals: ["public/**"],
+    junitReport: true,
+    onlyChanged: true,
+  });
 
-runValidation("PR workflow", {
-  projectId: "Project:...",
-  autoAcceptChanges: "main",
-  exitOnceUploaded: true,
-  externals: ["public/**"],
-  onlyChanged: true,
-  skip: "dependabot/**",
-});
+  await runValidation("PR workflow", {
+    projectId: "Project:...",
+    autoAcceptChanges: "main",
+    exitOnceUploaded: true,
+    externals: ["public/**"],
+    onlyChanged: true,
+    skip: "dependabot/**",
+  });
 
-runValidation("string-or-boolean type", {
-  projectId: "Project:...",
-  autoAcceptChanges: "main",
-  exitOnceUploaded: true,
-  externals: ["public/**"],
-  onlyChanged: "my-folder/**",
-  skip: "dependabot/**",
-});
+  await runValidation("string-or-boolean type", {
+    projectId: "Project:...",
+    autoAcceptChanges: "main",
+    exitOnceUploaded: true,
+    externals: ["public/**"],
+    onlyChanged: "my-folder/**",
+    skip: "dependabot/**",
+  });
 
-runValidation("Exhaustive", {
-  projectId: "Project:...",
-  onlyChanged: true,
-  skip: "dependabot/**",
-  onlyStoryFiles: ["src/ui/**"],
-  onlyStoryNames: ["Atoms/Button/*"],
-  traceChanged: false,
-  untraced: ["my-folder/**"],
-  externals: ["public/**"],
-  debug: true,
-  diagnosticsFile: false,
-  fileHashing: true,
-  junitReport: true,
-  zip: true,
-  autoAcceptChanges: "main",
-  exitZeroOnChanges: "!(main)",
-  exitOnceUploaded: true,
-  ignoreLastBuildOnBranch: "my-branch",
-  buildScriptName: "build:storybook",
-  playwright: true,
-  cypress: false,
-  outputDir: "storybook-static",
-  skipUpdateCheck: false,
-  storybookBuildDir: "storybook-static",
-  storybookBaseDir: "src/ui",
-  storybookConfigDir: ".storybook",
-  storybookLogFile: "sb.txt",
-  logFile: "logs.txt",
-  uploadMetadata: true,
-});
+  await runValidation("Exhaustive", {
+    projectId: "Project:...",
+    onlyChanged: true,
+    skip: "dependabot/**",
+    onlyStoryFiles: ["src/ui/**"],
+    onlyStoryNames: ["Atoms/Button/*"],
+    traceChanged: false,
+    untraced: ["my-folder/**"],
+    externals: ["public/**"],
+    debug: true,
+    diagnosticsFile: false,
+    fileHashing: true,
+    junitReport: true,
+    zip: true,
+    autoAcceptChanges: "main",
+    exitZeroOnChanges: "!(main)",
+    exitOnceUploaded: true,
+    ignoreLastBuildOnBranch: "my-branch",
+    buildScriptName: "build:storybook",
+    playwright: true,
+    cypress: false,
+    outputDir: "storybook-static",
+    skipUpdateCheck: false,
+    storybookBuildDir: "storybook-static",
+    storybookBaseDir: "src/ui",
+    storybookConfigDir: ".storybook",
+    storybookLogFile: "sb.txt",
+    logFile: "logs.txt",
+    uploadMetadata: true,
+  });
 
-runValidation("With deprecated projectToken", {
-  projectId: "Project:...",
-  projectToken: "xxxx",
-  onlyChanged: true,
-  skip: "dependabot/**",
-  onlyStoryFiles: ["src/ui/**"],
-  onlyStoryNames: ["Atoms/Button/*"],
-  traceChanged: false,
-  untraced: ["my-folder/**"],
-  externals: ["public/**"],
-  debug: true,
-  diagnosticsFile: false,
-  fileHashing: true,
-  junitReport: true,
-  zip: true,
-  autoAcceptChanges: "main",
-  exitZeroOnChanges: "!(main)",
-  exitOnceUploaded: true,
-  ignoreLastBuildOnBranch: "my-branch",
-  buildScriptName: "build:storybook",
-  playwright: true,
-  cypress: false,
-  outputDir: "storybook-static",
-  skipUpdateCheck: false,
-  storybookBuildDir: "storybook-static",
-  storybookBaseDir: "src/ui",
-  storybookConfigDir: ".storybook",
-  storybookLogFile: "sb.txt",
-  logFile: "logs.txt",
-  uploadMetadata: true,
-});
+  await runValidation("With deprecated projectToken", {
+    projectId: "Project:...",
+    projectToken: "xxxx",
+    onlyChanged: true,
+    skip: "dependabot/**",
+    onlyStoryFiles: ["src/ui/**"],
+    onlyStoryNames: ["Atoms/Button/*"],
+    traceChanged: false,
+    untraced: ["my-folder/**"],
+    externals: ["public/**"],
+    debug: true,
+    diagnosticsFile: false,
+    fileHashing: true,
+    junitReport: true,
+    zip: true,
+    autoAcceptChanges: "main",
+    exitZeroOnChanges: "!(main)",
+    exitOnceUploaded: true,
+    ignoreLastBuildOnBranch: "my-branch",
+    buildScriptName: "build:storybook",
+    playwright: true,
+    cypress: false,
+    outputDir: "storybook-static",
+    skipUpdateCheck: false,
+    storybookBuildDir: "storybook-static",
+    storybookBaseDir: "src/ui",
+    storybookConfigDir: ".storybook",
+    storybookLogFile: "sb.txt",
+    logFile: "logs.txt",
+    uploadMetadata: true,
+  });
 
-runValidation("With schema defined inline", {
-  $schema: "https://www.chromatic.com/config-file.schema.json",
-  projectId: "Project:...",
-});
+  await runValidation("With schema defined inline", {
+    $schema: "https://www.chromatic.com/config-file.schema.json",
+    projectId: "Project:...",
+  });
+}
